d7c1c42cf137a461bf4becc48a0773212e8a9075
Jr Jmnz <klys2007@gmail.com>
2020-05-09 13:43:17 -0400

    New index with last 3 projectz

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
     include_once("settings.php");
     $url = "{$api}/home";
     $data = json_decode(file_get_contents($url));
+
+    $lastProjs = json_decode(file_get_contents($api."/projects?_limit=3&_sort=startDate:DESC"));
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,25 +31,28 @@
                 <p class="lead"><strong><?= $data->subdescription2 ?></strong></p>
                 <p><a class="btn btn-lg btn-success" href="about.php" role="button">Read more</a></p>
             </div>
-            <h3>Lastest News &amp; Projects @ #klys</h3>
-            <div class="marketing row">
-                <div class="col-lg-6">
-                    <h4>Flux in React</h4>
-                    <p>A new way to develop on react with a more lineal advance method.</p>
-                    <h4>Best server lenguage</h4>
-                    <p>It may be php, or python, or go, in this topic we talk about our review and experience in server software developing</p>
-                    <h4>Marketing during quarentine</h4>
-                    <p>Is really the fear so powerfull when moving crows for market motivation, the numbers talk by itself...</p>
-                </div>
-                <div class="col-lg-6">
-                    <h4>Nova</h4>
-                    <p>Nova is a real time action game where you are part of a galactic spaceship team fighting against another team for destroying the headquarter to win.</p>
-                    <h4>Block Rusher</h4>
-                    <p>A mobile game to avoid obstacles</p>
-                    <h4>Nations</h4>
-                    <p>A war game web based for nation administration to war managing</p>
-                </div>
+            
+            <h3>Lastest Projects @ #klysDev</h3>
+            <?php foreach($lastProjs as $key => $value) { 
+
+                $imgUrl = "imgs/noimage.png";
+                if ($value->image != null) {
+                    $imgUrl = $api.$value->image->url;
+                }
+                $imgType = explode(".", $imgUrl)[1];
+                $imgData = "data:image/".$imgType.";base64,".base64_encode(file_get_contents($imgUrl));
+
+                ?>
+
+            <div class="media"> 
+                <img class="d-flex mr-3" src="<?= $imgData ?>" alt="Generic placeholder image" width="150"> 
+                <div class="media-body"> 
+                    <h5 class="mt-0"><?= $value->title ?></h5>  
+                    <?= $value->short_description ?>                    
+                </div>                 
             </div>
+
+            <?php } ?>
             <?php include("footer.php"); ?>
         </div>         
         <!-- /container -->
