3b48693ea963804a23e6a689dfea2671e5970840
Jr Jmnz <klys2007@gmail.com>
2020-05-10 03:20:54 -0400

    Google Analytics

--- header.php
+++ header.php
@@ -7,3 +7,12 @@
 <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
 <!-- Custom styles for this template -->
 <link href="jumbotron-narrow.css" rel="stylesheet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-104069212-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-104069212-3');
+</script>
